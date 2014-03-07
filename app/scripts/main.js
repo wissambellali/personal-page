956cf9b96c36a9c02748e43c864cc28f5b33f61a
wissam bellali <wissam.bellali@gmail.com>
2014-03-07 15:51:18 -0500

    hide submenu & linkable span

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+  $(document).ready( function () {
+
+    // On cache les sous-menus :
+    $(".navigation ul.subMenu").hide();
+
+    // On sélectionne tous les items de liste portant la classe "toggleSubMenu"
+    // et on remplace l'élément span qu'ils contiennent par un lien :
+    $(".navigation li.toggleSubMenu span").each( function () {
+        $(this).replaceWith('<a href="" title="Afficher le sous-menu">' + $(this).text() + '<\/a>') ;
+    } ) ;    
+
+} ) ;
