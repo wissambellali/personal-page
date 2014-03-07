795b1bba62d0a30fd6d23654b1541a3324fea9fe
wissam bellali <wissam.bellali@gmail.com>
2014-03-07 17:57:39 -0500

    no modification

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,4 +9,20 @@
         $(this).replaceWith('<a href="" title="Afficher le sous-menu">' + $(this).text() + '<\/a>') ;
     } ) ;    
 
+        // On modifie l'évènement "click" sur les liens dans les items de liste
+    // qui portent la classe "toggleSubMenu" :
+    $(".navigation li.toggleSubMenu > a").click( function () {
+        // Si le sous-menu était déjà ouvert, on le referme :
+        if ($(this).next("ul.subMenu:visible").length != 0) {
+            $(this).next("ul.subMenu").slideUp("normal");
+        }
+        // Si le sous-menu est caché, on ferme les autres et on l'affiche :
+        else {
+
+            $(this).next("ul.subMenu").slideDown("normal");
+        }
+        // On empêche le navigateur de suivre le lien :
+        return false;
+    });  
+
 } ) ;
